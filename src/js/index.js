b5840c7c2ce61cb95017398970fccfc958d2de27
andresciceri <andresciceri@gmail.com>
2018-06-27 18:36:51 -0500

    update: routes functions & controller functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,107 +1,98 @@
-var  routes = [
-        {
-           "path": "/",
-           "component": "list.html",
-           "callback": function() {
-                $.getJSON('./js/books.json').done(function(response){        
-            
-                    let items = response.items;
-                    let template;
-                    $.ajax(
-                        {
-                            url: './components/templates/card.html',
-                            type: 'GET',
-                            dataType: 'text',
-                            success: function(response){                
-                                template = response;                     
-            
-                                for(let i = 0; i < items.length; i++){
-                                    newTemplate = template.slice(0);
-                                    let volInfo = items[i].volumeInfo; 
-                                    let id = items[i].id;                       
-                                    let keys = Object.keys(volInfo);                          
-                                    let link = "#/detail/"+id;                        
-                                    newTemplate = newTemplate.replace("{{routeLink}}", link).slice(0);
-            
-                                    for(let j=0; j < keys.length; j++){                                                      
-                                        if(keys[j] == "imageLinks"){                                
-                                            let st = JSON.stringify(volInfo[keys[j]].smallThumbnail);
-                                            newTemplate = newTemplate.replace("{{"+ keys[j] +"}}", st).slice(0);
-                                        }
-                                        else{                                 
-                                            let st = JSON.stringify(volInfo[keys[j]]);
-                                            newTemplate = newTemplate.replace("{{"+ keys[j] +"}}", st).slice(0);                                                                
-                                        }                                 
-                                        if(j == (keys.length-1)){
-                                            $("#books").append(newTemplate);        
-                                        }
-                                    }                                                            
-                                }                    
-                            },
-                            error: function(error){
-                                console.log(error);
-                            },
-                            complete: function(xhr, status){
-                                console.log(status);
+var routes = [
+    {
+        "path": "/",
+        "component": "list.html",
+        "controller": function(){
+            $.getJSON('./js/books.json').done(function(response){
+                let items = response.items;
+                let template;
+
+                $.ajax(
+                    {
+                    url: './components/templates/card.html',
+                    type: 'GET',
+                    dataType: 'text',
+                    success: function(response){
+                        template = response;
+                        for(let i=0; i < items.length; i++){
+                            let newTemplate = template.slice(0);
+                            let volInfo = items[i].volumeInfo;
+                            let id = items[i].id;
+
+                            let keys = Object.keys(volInfo);
+                            let link = "#/detail/"+id;
+
+                            newTemplate = newTemplate.replace("{{routeLink}}", link).slice(0);
+
+                            for(let j=0; j < keys.length; j++){
+                                if(keys[j] == 'imageLinks'){
+                                    let urlImage =  volInfo[keys[j]].smallThumbnail;
+                                    newTemplate = newTemplate.replace("{{" + keys[j] +"}}", urlImage).slice(0);
+                                }else {
+                                    let textBook = volInfo[keys[j]];
+                                    newTemplate = newTemplate.replace("{{" + keys[j] +"}}", textBook).slice(0);
+                                }
+                            }
+
+                            $("#books").append(newTemplate);
+                        }
+                    },
+                    error: function(error){
+                        console.log(error);
+                    },
+                    complete: function(xhr, status){
+                        console.log(status);
+                    }
+
+                });            
+            })
+        }
+    },
+    {
+        "path": "/detail/:id",
+        "component": "detail.html",
+        "controller": function(id){
+            $.getJSON('./js/books.json').done(function(response){
+                let items = response.items;
+                let item = items.find(function(elem){
+                    return elem.id == id;
+                });
+
+                let template;
+
+                $.ajax({
+                    url: './components/templates/book.html',
+                    type: 'GET',
+                    dataType: 'text',
+                    success: function(response){
+                        template = response;                                                                                                                 
+                        let newTemplate = template.slice(0);
+                        let volInfo = item.volumeInfo;                                
+                        let keys = Object.keys(volInfo);    
+                        for(let j=0; j < keys.length; j++){
+                            if(keys[j] == 'imageLinks'){
+                                let urlImage = volInfo[keys[j]].smallThumbnail;
+                                newTemplate = newTemplate.replace("{{" + keys[j] + "}}", urlImage).slice(0); 
+                            }else {
+                                let textBook = volInfo[keys[j]];
+                                newTemplate = newTemplate.replace("{{" + keys[j] + "}}", textBook).slice(0); 
                             }
                         }
-                    );        
-            
-            
-                }); 
-           }    
-        },
-        {
-            "path": "/detail/:id",
-            "component": "detail.html",
-            "callback": function(id){
-                $.getJSON('./js/books.json').done(function(response){        
-            
-                    let items = response.items;
-                    for(let i=0; i < items.length; i++){
-                        if(items[i].id == id){
-                            let item = items[i];                            
-                            let template;
-                                $.ajax(
-                                    {
-                                        url: './components/templates/book.html',
-                                        type: 'GET',
-                                        dataType: 'text',
-                                        success: function(response){                
-                                            template = response;                                                                                         
-                                            newTemplate = template.slice(0);
-                                            let volInfo = item.volumeInfo; 
-                                            let id = item.id;                       
-                                            let keys = Object.keys(volInfo);                                                                                                                                                
-                    
-                                            for(let j=0; j < keys.length; j++){                                                      
-                                                if(keys[j] == "imageLinks"){                                                                                   
-                                                    let st = JSON.stringify(volInfo[keys[j]].smallThumbnail);                                                    
-                                                    newTemplate = newTemplate.replace("{{"+ keys[j] +"}}", st).slice(0);
-                                                }
-                                                else{                                 
-                                                    let st = JSON.stringify(volInfo[keys[j]]);
-                                                    newTemplate = newTemplate.replace("{{"+ keys[j] +"}}", st).slice(0);                                                                
-                                                }                                 
-                                                if(j == (keys.length-1)){
-                                                    $("#book").append(newTemplate);        
-                                                }
-                                            }                                                                                                        
-                                        },
-                                        error: function(error){
-                                            console.log(error);
-                                        },
-                                        complete: function(xhr, status){
-                                            console.log(status);
-                                        }
-                                    }
-                                ); 
-                        }
-                    }            
-                }); 
-            }
-        } 
-];
+
+                        $('#book').append(newTemplate);
+                    },
+                    error: function(error){
+                        console.log(error);
+                    },
+                    complete: function(xhr, status){
+                        console.log(status);
+                    }
+
+            });
+        });
+        }
+    }
+]
 
 
 
@@ -134,22 +125,7 @@ $(document).ready(function(){
     
     $(window).on('hashchange', function(e){
         let event = e.originalEvent;
-        let hash = event.newURL.split('#')[1];
-
-        routes.map(function(data){        
-            var url = hash || '/';                                      
-            if(url == "/" && data.path === "/"){            
-                getContent("./components/" + data.component, data.callback);                        
-            }else {            
-                let paths = url.split("/"); 
-                console.log(paths);
-                let size = paths.length;
-                let dataPath = data.path.split("/");
-                if(size === dataPath.length){
-                    getContent("./components/" + data.component, data.callback, paths[2]);
-                }            
-            }       
-        });      
+        router(event.target.location);              
     });    
     
 });
@@ -160,19 +136,31 @@ function router(window){
     let hash = location.hash.split("#")[1];    
 
     routes.map(function(data){        
-        var url = location.hash.slice(1) || '/';              
-        console.log(url);
+        var url = location.hash.slice(1) || '/';     
+        let parts = url.substr(1).split('/'), param;
+        
         if(url == "/" && data.path === "/"){            
-            getContent("./components/" + data.component, data.callback);                        
-        }else {            
-            let paths = url.split("/"); 
-            console.log(paths);
-            let size = paths.length;
-            let dataPath = data.path.split("/");
-            if(size === dataPath.length){
-                getContent("./components/" + data.component, data.callback, paths[2]);
-            }            
-        }       
+            getContent("./components/" + data.component, data.controller);                        
+        }else if(data.path.match(/:id/g)) {            
+            let mod = data.path.split('/:id')[0].slice(1);
+            while(parts.length){
+                if(parts.shift() == mod){
+                    param = parts.shift();
+                    getContent("./components/" + data.component, data.controller, param);
+                }else {
+                    parts.shift();
+                }
+            }
+        }else {
+            let mod = data.path.slice(1);
+            while(parts.length){
+                if(parts.shift() === mod) {                    
+                    getContent("./components/" + data.component, data.controller);
+                }else {
+                    parts.shift();
+                }
+            }  
+        }    
     });    
 }
 
